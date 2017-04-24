5cd0a2b51207df4eff2393e9b9b55d42ba71e126
Maksim Kostromin <daggerok@gmail.com>
2017-04-24 06:24:58 +0300

    move installation details into scripts

---
+++ scripts/install-manager.bash
@@ -0,0 +1,7 @@
+#!/bin/bash
+apt update
+apt install -y iputils-ping openssh-client net-tools curl software-properties-common vim
+apt-add-repository ppa:ansible/ansible -y
+apt update
+apt install -y ansible
+/bin/bash
---
+++ scripts/install-node.bash
@@ -0,0 +1,6 @@
+#!/bin/bash
+apt update
+apt install -y iputils-ping openssh-server net-tools curl vim python
+sed -i 's/PermitRootLogin prohibit-password/PermitRootLogin yes/' /etc/ssh/sshd_config
+service ssh start
+/bin/bash
